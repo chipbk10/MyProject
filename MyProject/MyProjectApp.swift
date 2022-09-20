aca4826d3ca5d635cf8bee7ffba983b2075a4977
Hieu Luong <dinh.hieu.luong@ing.com>
2022-09-20 15:03:09 +0200

    Move to SwiftUI project

---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by Hieu Luong on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
